ea2061c4709a064b85448391ffc1a5f7343b6b18
Kamal Mohammed <kamal.mohammed@gov.bc.ca>
2020-03-10 07:16:03 -0700

    Initial commit

---
+++ api/src/main/java/ca/bc/gov/educ/studentreport/StudentReportApiApplication.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.educ.studentreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentReportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentReportApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/ca/bc/gov/educ/studentreport/StudentReportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.educ.studentreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentReportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
