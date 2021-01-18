438373176cafd54090ee71b98580c3c611e2edf6
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-01-18 15:38:28 -0800

    Changes in template

---
+++ api/src/main/java/ca/bc/gov/educ/api/report/dto/CodeDTO.java
@@ -0,0 +1,10 @@
+package ca.bc.gov.educ.api.report.dto;
+
+import lombok.Data;
+
+@Data
+public class CodeDTO {
+
+	private String code;
+	private String name;
+}
---
+++ api/src/main/java/ca/bc/gov/educ/api/report/dto/ReasonDTO.java
@@ -0,0 +1,9 @@
+package ca.bc.gov.educ.api.report.dto;
+
+import lombok.Data;
+
+@Data
+public class ReasonDTO {
+
+	private String reason;
+}
